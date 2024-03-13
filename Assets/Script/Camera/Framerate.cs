5f5c7b71b7f8a90cb3a7f8e5cc6d26728166cb37
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-03-13 16:51:25 +0100

    drunk effect

---
+++ Assets/Script/Camera/Framerate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Framerate : MonoBehaviour
+{
+    private void Start()
+    {
+        Application.targetFrameRate = 20;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Camera cam = GetComponent<Camera>();
+
+        RenderTexture m_SavedTexture = cam.targetTexture;
+
+        if (m_SavedTexture == null)
+            m_SavedTexture = Instantiate(source) as RenderTexture;
+
+        if (Time.frameCount % 20 == 0)
+            Graphics.Blit(source, m_SavedTexture);
+
+        Graphics.Blit(m_SavedTexture, destination);
+    }
+}
